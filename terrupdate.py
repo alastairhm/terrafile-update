da41b0dc67bb49c90e3297a534078c14a55f88a7
Alastair Montgomery <alastair.montgomery@engineering.digital.dwp.gov.uk>
2022-03-24 11:28:13 +0000

    Return result 0 no changes, 1 changes found

--- terrupdate.py
+++ terrupdate.py
@@ -2,6 +2,7 @@
 """Update Terrafile to latest versions."""
 
 import os
+import sys
 import json
 import argparse
 import urllib3
@@ -12,6 +13,16 @@ def major_update(current, new):
     """Check if major version bump."""
     status = current.split('.')[0] != new.split('.')[0]
     return status
+
+def generate_url(details):
+    """Format URL for API call"""
+    source = details["source"].split('/')
+    tail = source[3]
+    for elem in source[4:]:
+        tail = tail + '%2F' + elem
+    api = 'https://'+source[2]+'/api/v4/projects/'+tail+'/repository/tags'
+    return api
+
 
 def main():
     """Update Terrafile to latest versions."""
@@ -29,7 +40,8 @@ def main():
     parser.add_argument("-v","--verbose", action='store_true',help="Verbose output")
     parser.add_argument("-c","--check", action='store_true',help="Just check no output")
     args = parser.parse_args()
-    if args.verbose: print("Arguements :",args)
+    if args.verbose:
+        print("Arguements :",args)
 
     if os.path.isfile(args.input):
         terrafile = []
@@ -44,12 +56,8 @@ def main():
             if args.verbose:
                 print('Repo [',repo,']')
             details = terrafile[repo]
-            source = details["source"].split('/')
-            tail = source[3]
-            for elem in source[4:]:
-                tail = tail + '%2F' + elem
-            api = 'https://'+source[2]+'/api/v4/projects/'+tail+'/repository/tags'
             version = details["version"]
+            api = generate_url(details)
             response = http.request(
                 'GET',
                 api,
@@ -63,7 +71,7 @@ def main():
                 major_bump = major_update(version,tags[0]["name"])
                 if not major_bump or args.major:
                     if args.verbose:
-                       print(version,'-->',tags[0]["name"])
+                        print(version,'-->',tags[0]["name"])
                     terrafile[repo]["version"] = tags[0]["name"]
                     dirty = True
                 else:
@@ -77,13 +85,17 @@ def main():
                     output_file.write(yaml.dump(terrafile))
             else:
                 print("Updates detected.")
+                return 1
         else:
             print('No changes required.')
     else:
         print('Error : Input file not found')
+        return 1
+    return 0
 
 if __name__ == "__main__":
     try:
-        main()
+        sys.exit(main())
     except Exception as e:
         print(e)
+        sys.exit(1)
